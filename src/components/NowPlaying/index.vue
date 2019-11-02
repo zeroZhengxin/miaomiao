0c4e01ff46acbe2d56a61b53ff6ec0769ca100fc
zeroZhengxin <1104701006@qq.com>
2019-11-02 16:33:58 +0800

    11.2

--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="movie_body">
+    <div class="movie_body" ref="movie_body">
+        <Loading v-if="isLoading"/>
+        <Scroller v-else :handleToScroll="handleToScroll" :handleToTouchEnd="handleToTouchEnd">
         <ul>
             <!-- <li>
                 <div class="pic_show"><img src="/images/movie_1.jpg"></div>
@@ -13,8 +15,9 @@
                     购票
                 </div>
             </li> -->
+            <li class="pullDown">{{ pullDownMsg }}</li>
             <li v-for="item in movieList" :key="item.id">
-                <div class="pic_show">
+                <div class="pic_show" @tap="handleToDetail">
                     <img :src="item.img | setWH('128.180')">
                 </div>
                 <div class="info_list">
@@ -31,23 +34,89 @@
                 </div>
             </li>
         </ul>
+        </Scroller>
     </div>
 </template>
 <script>
+// import BScroll from 'better-scroll';
 export default {
     name:'NowPlaying',
     data(){
         return{
-            movieList:[]
+            movieList:[],
+            pullDownMsg:'',
+            isLoading: true,
+            prevCityId: -1
         }
     },
-    mounted(){
-        this.axios.get('/api/movieOnInfoList?cityId=10').then((res)=>{
+    activated(){ //keep-alive组件激活时调用
+        // console.log('123');
+        var cityId = this.$store.state.city.id;
+        if(this.prevCityId === cityId){ return;}
+        this.isLoading = true;
+        // console.log('123');
+
+        this.axios.get('/api/movieOnInfoList?cityId='+cityId).then((res)=>{
             var msg = res.data.msg;
             if( msg === 'ok'){
                 this.movieList = res.data.data.movieList;
+                this.isLoading = false;
+                this.prevCityId = cityId;
+                //界面渲染完毕之后才会触发方法的回调
+                // this.$nextTick(()=>{
+                //     var scroll = new BScroll(this.$refs.movie_body,{
+                //         tap : true,
+                //         probeType:1
+                //     });
+                //     scroll.on('scroll',(pos)=>{
+                //         //console.log('b');
+                //         //pos.y滑动的纵轴
+                //         if(pos.y>30){
+                //             this.pullDownMsg = '正在更新中';
+                //         }
+                //     });
+                //     scroll.on('touchEnd',(pos)=>{
+                //         // console.log('c');
+                //         if(pos.y>30){
+                //             this.axios.get('/api/movieOnInfoList?cityId=11').then((res)=>{
+                //                 var msg = res.data.msg;
+                //                 if( msg === 'ok'){
+                //                     this.pullDownMsg = '更新成功';
+                //                     setTimeout(() => {
+                //                         this.movieList = res.data.data.movieList;
+                //                         this.pullDownMsg = '';
+                //                     }, 1000); 
+                //                 }
+                //             });
+                //         }
+                //     });
+                // });    
             }
         });
+    },
+    methods:{
+        handleToDetail(){
+            console.log('a');
+        },
+        handleToScroll(pos){
+            if(pos.y > 30){
+                this.pullDownMsg = '正在更新...';
+            }
+        },
+        handleToTouchEnd(pos){
+            if(pos.y > 30){
+                this.axios.get('/api/movieOnInfoList?cityId=11').then((res)=>{
+                    var msg = res.data.msg;
+                    if( msg === 'ok'){
+                        this.pullDownMsg = '更新成功';
+                        setTimeout(()=>{
+                            this.movieList = res.data.data.movieList;
+                            this.pullDownMsg = ''
+                        },1000);
+                    }
+                });
+            }
+        }
     }
 }
 </script>
@@ -64,4 +133,5 @@ export default {
 .movie_body .info_list img{ width:50px; position: absolute; right:10px; top: 5px;}
 .movie_body .btn_mall , .movie_body .btn_pre{ width:47px; height:27px; line-height: 28px; text-align: center; background-color: #f03d37; color: #fff; border-radius: 4px; font-size: 12px; cursor: pointer;}
 .movie_body .btn_pre{ background-color: #3c9fe6;}
+.movie_body .pullDown{ margin:0; padding:0; border: none;}
 </style>
--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -52,9 +52,10 @@ export default {
             // console.log(newVal);
             //函数防抖策略
             var that = this;
+            var cityId = this.$store.state.city.id;
             this.cancelRequest();
 
-            this.axios.get('/api/searchList?cityId=10&kw='+newVal,{
+            this.axios.get('/api/searchList?cityId='+cityId+'&kw='+newVal,{
                 cancelToken: new this.axios.CancelToken(function executor(c){
                     that.source = c;
                 })
